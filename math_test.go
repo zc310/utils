17c9bfea451964703311d91bf760bb8858a02cb6
zc310 <liushiyan@msn.com>
2017-12-22 11:02:35 +0800

    调整 使用stretchr/testify

--- math_test.go
+++ math_test.go
@@ -3,18 +3,18 @@ package utils
 import (
 	"testing"
 
-	. "gopkg.in/go-playground/assert.v1"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_Math(t *testing.T) {
-	Equal(t, 0, StrToInt("0"))
-	Equal(t, -1, StrToInt("-1"))
-	Equal(t, 0, StrToInt(""))
-	Equal(t, float32(0), StrToFloat32(""))
-	Equal(t, float32(0), Abs32(0))
-	Equal(t, float32(1), Abs32(-1))
-	Equal(t, float32(1.01), StrToFloat32Def("-", float32(1.01)))
-	Equal(t, float32(1.02), StrToFloat32Def("1.02", float32(1.01)))
-	Equal(t, 0, StrToIntDef("", 0))
-	Equal(t, 123, StrToIntDef("123", 0))
+	assert.Equal(t, 0, StrToInt("0"))
+	assert.Equal(t, -1, StrToInt("-1"))
+	assert.Equal(t, 0, StrToInt(""))
+	assert.Equal(t, float32(0), StrToFloat32(""))
+	assert.Equal(t, float32(0), Abs32(0))
+	assert.Equal(t, float32(1), Abs32(-1))
+	assert.Equal(t, float32(1.01), StrToFloat32Def("-", float32(1.01)))
+	assert.Equal(t, float32(1.02), StrToFloat32Def("1.02", float32(1.01)))
+	assert.Equal(t, 0, StrToIntDef("", 0))
+	assert.Equal(t, 123, StrToIntDef("123", 0))
 }
--- time_test.go
+++ time_test.go
@@ -4,20 +4,20 @@ import (
 	"testing"
 	"time"
 
-	. "gopkg.in/go-playground/assert.v1"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_Time(t *testing.T) {
 	t1, err := TimetampToTime("1352397861001")
-	Equal(t, err, nil)
-	Equal(t, t1, time.Date(2012, 11, 8, 18, 4, 21, int(time.Millisecond), time.UTC))
+	assert.Equal(t, err, nil)
+	assert.Equal(t, t1, time.Date(2012, 11, 8, 18, 4, 21, int(time.Millisecond), time.UTC))
 
 	t1, err = TimetampToTime("1352397861")
-	Equal(t, err, nil)
-	Equal(t, t1, time.Date(2012, 11, 8, 18, 4, 21, 0, time.UTC))
+	assert.Equal(t, err, nil)
+	assert.Equal(t, t1, time.Date(2012, 11, 8, 18, 4, 21, 0, time.UTC))
 
 	t1, err = TimetampToTime("1351700038292387000")
-	Equal(t, err, nil)
-	Equal(t, t1, time.Date(2012, 10, 31, 16, 13, 58, 292387000, time.UTC))
+	assert.Equal(t, err, nil)
+	assert.Equal(t, t1, time.Date(2012, 10, 31, 16, 13, 58, 292387000, time.UTC))
 
 }
