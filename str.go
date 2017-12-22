6d25b4bcc00f02734f1ab793d4241898f6dab5fa
zc310 <liushiyan@msn.com>
2017-12-22 11:01:49 +0800

    新增 支持整形数组转逗号分割字符串

--- str.go
+++ str.go
@@ -1,9 +1,23 @@
 package utils
 
+import (
+	"strconv"
+	"strings"
+)
+
 // IfEmpty return first non-empty string
 func IfEmpty(a, b string) string {
 	if a == "" {
 		return b
 	}
 	return a
+}
+
+// DynArrayIntToCommaText return  CommaText
+func DynArrayIntToCommaText(a []int) string {
+	var s []string
+	for i := 0; i < len(a); i++ {
+		s = append(s, strconv.Itoa(a[i]))
+	}
+	return strings.Join(s, ",")
 }
